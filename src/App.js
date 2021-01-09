1dee9b8413d6e9f8c2e8e3ab16b1f5419e919e58
Efe Bikmaz <bikmazefe@gmail.com>
2021-01-09 22:11:10 +0300

    tweaks for github pages

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename = "/">
         <Nav cartSize = {cartItems.length} />
         <Switch>
           <Route exact path = "/" component = {Home}/>
